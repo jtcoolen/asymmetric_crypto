66999af7afd745f0d629225dab95632c1f7e7f08
Julien Coolen <>
2022-01-02 00:30:24 +0100

    working baby step giant step

--- discrete_log/discrete_log.c
+++ discrete_log/discrete_log.c
@@ -1,3 +1,4 @@
+#include <blake2b.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,62 +6,73 @@
 
 #include <discrete_log.h>
 #include <hashmap.h>
+//#include <uthash.h>
 
 struct babystep_pair {
   mpz_t exponent; // a
   mpz_t power;    // g^a
 };
 
+char *hash(const mpz_t a) {
+  size_t s = mpz_sizeinbase(a, 10);
+  char *str = malloc(s + 1);
+  gmp_snprintf(str, s + 1, "%Zd", a);
+  return str;
+}
+
 void babystep_giantstep(mpz_t cyclic_group_order, mpz_t generator, mpz_t h,
                         mpz_t log) {
   mpz_t group_order_sqrt, group_order_sqrt_neg, a, j, gen_pow, h_pow;
   mpz_inits(group_order_sqrt, group_order_sqrt_neg, a, j, gen_pow, h_pow, NULL);
 
-  mpz_sqrt(group_order_sqrt, cyclic_group_order);
+  mpf_t sqrt;
+  mpf_init(sqrt);
+  mpf_set_z(sqrt, cyclic_group_order);
+  mpf_sqrt(sqrt, sqrt);
+  mpf_ceil(sqrt, sqrt);
+  mpz_set_f(group_order_sqrt, sqrt);
 
   HASHMAP(char, struct babystep_pair) babystep_pairs;
-
   hashmap_init(&babystep_pairs, hashmap_hash_string, strcmp);
 
   mpz_set_ui(a, 1);
-  char key[64];
+
   struct babystep_pair *p;
-  while (mpz_cmp(j, group_order_sqrt) < 0) {
-    memset(key, 0, 64);
-    printf("len=%d", gmp_snprintf(key, 64, "%Za", a));
+  while (mpz_cmp(j, group_order_sqrt) <= 0) {
     p = malloc(sizeof *p);
     mpz_set(p->exponent, j);
     mpz_set(p->power, a);
-    hashmap_put(&babystep_pairs, key, p);
+
+    hashmap_put(&babystep_pairs, hash(a), p);
+
     mpz_mul(a, a, generator);
     mpz_mod(a, a, cyclic_group_order);
     mpz_add_ui(j, j, 1);
   }
 
-  const char *k;
+  /*const mpz_t *k;
 
   hashmap_foreach(k, p, &babystep_pairs) {
-    gmp_printf("\npairs[%s]: j= %Zd ; pow = %Zd \n", key, p->exponent, p->power);
-  }
+    gmp_printf("\npairs: j= %Zd ; pow = %Zd \n", p->exponent, p->power);
+  }*/
 
   mpz_neg(group_order_sqrt_neg, group_order_sqrt);
-  mpz_powm(gen_pow, a, group_order_sqrt_neg, cyclic_group_order);
+  mpz_powm(gen_pow, generator, group_order_sqrt_neg, cyclic_group_order);
 
   mpz_set(h_pow, h);
   mpz_set_ui(j, 0);
   while (mpz_cmp(j, group_order_sqrt) < 0) {
-    memset(key, 0, 64);
-    gmp_printf("%Za \n", h_pow);
-    printf("len=%d", gmp_snprintf(key, 64, "%Za", h_pow));
-    p = hashmap_get(&babystep_pairs, key);
+    p = hashmap_get(&babystep_pairs, hash(h_pow));
     if (p) {
-      printf("\n");
-      mpz_out_str(stdout, 10, p->power);
+      mpz_out_str(stdout, 10, p->exponent);
       mpz_mul(j, j, group_order_sqrt);
       mpz_add(log, j, p->exponent);
+      hashmap_cleanup(&babystep_pairs);
       return;
     }
     mpz_mul(h_pow, h_pow, gen_pow);
+    mpz_mod(h_pow, h_pow, cyclic_group_order);
     mpz_add_ui(j, j, 1);
   }
+  hashmap_cleanup(&babystep_pairs);
 }
--- discrete_log/discrete_log.h
+++ discrete_log/discrete_log.h
@@ -3,6 +3,7 @@
 
 #include <gmp.h>
 
-void babystep_giantstep(mpz_t cyclic_group_order, mpz_t generator, mpz_t h, mpz_t log);
+void babystep_giantstep(mpz_t cyclic_group_order, mpz_t generator, mpz_t h,
+                        mpz_t log);
 
 #endif
